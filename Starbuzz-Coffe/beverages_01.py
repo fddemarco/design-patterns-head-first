f8d98adfd1a2fd91010694034c19ee6cd991fcc7
FrancoD <franco.demarco3400@gmail.com>
2024-07-13 22:35:39 -0300

    Starbuzz coffee

---
+++ Starbuzz-Coffe/beverages_01.py
@@ -0,0 +1,59 @@
+import abc
+
+class Beverage(abc.ABC):
+    """ Beverage is an abstract class, subclassed by all beverages offered
+    in the coffee shop.
+
+    Attributes
+    ----------
+    description : str
+        The description instance variable is set in each subclass and holds a
+        description of the bevreage, like "Most Excellent Dark Roast".
+    """
+
+    def __init__(self, description: str):
+        self.description = description
+
+    def get_description(self) -> str:
+        """The get_description() method returns the description.
+
+        Returns
+        -------
+        str
+            _description_
+        """        
+        return self.description
+    
+    @abc.abstractmethod
+    def cost(self) -> float:
+        """The cost() method is abstract;
+        subclasses need to define their own implementation.
+
+        Returns
+        -------
+        float
+            beverage cost
+        """        
+        ...
+
+
+class HouseBlend(Beverage):
+    def cost(self):
+        ...
+
+
+class DarkRoast(Beverage):
+    def cost(self):
+        ...
+
+
+class Decaf(Beverage):
+    def cost(self):
+        ...
+
+
+class Esperesso(Beverage):
+    def cost(self):
+        ...
+
+
